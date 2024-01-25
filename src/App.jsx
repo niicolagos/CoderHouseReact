82227c0b49df7f62595504b6c24ac5ce9ed8254d
Nicolas Lagos <niicolagos@hotmail.com>
2024-01-24 22:25:30 -0300

    Proyecto de react , e-commerce

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import Product from "./components/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
